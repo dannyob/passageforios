18163f6285ec1f3bf002ec0809c18c6537df757d
Danny O'Brien <danny@spesh.com>
2025-12-30 15:57:04 -0800

    build: integrate sshage into gopenpgp iOS framework
    Add sshage/mobile package to the gopenpgp build so that Swift code can
    call the gomobile bindings for verifying age recipients changes.
    
    The build script now:
    1. Copies the sshage module into the gopenpgp checkout
    2. Removes the nested mobile/go.mod to make it part of the parent module
    3. Adds a replace directive so go can find the local sshage copy
    4. Adds sshage as a dependency and downloads all needed packages
    5. Includes sshage/mobile in the gomobile bind command

--- scripts/gopenpgp_build.sh
+++ scripts/gopenpgp_build.sh
@@ -16,7 +16,34 @@ mkdir -p "$CHECKOUT_PATH"
 
 git clone --depth 1 --branch "$GOPENPGP_VERSION" https://github.com/mssun/gopenpgp.git "$GOPENPGP_PATH"
 
+# Copy sshage module into gopenpgp checkout
+# The sshage module root is gopenpgp-additions/, containing go.mod and sshage/ subpackage
+SSHAGE_SRC="$(pwd)/scripts/gopenpgp-additions"
+SSHAGE_DEST="$GOPENPGP_PATH/sshage-module"
+cp -r "$SSHAGE_SRC" "$SSHAGE_DEST"
+
+# Remove the nested go.mod in mobile/ to make it part of the parent module
+# This allows gomobile to bind the mobile package as part of the sshage module
+rm -f "$SSHAGE_DEST/sshage/mobile/go.mod" "$SSHAGE_DEST/sshage/mobile/go.sum"
+
 pushd "$GOPENPGP_PATH"
+
+# Add sshage to go.mod with replace directive pointing to local copy
+cat >> go.mod <<'GOMOD'
+
+replace github.com/mssun/passforios/sshage => ./sshage-module
+GOMOD
+
+# Run go mod tidy first to update existing dependencies
+go mod tidy
+
+# Add sshage as a dependency after go mod tidy (so it doesn't get removed)
+# Then fetch the sshage subpackage to ensure its dependencies are in go.sum
+go get github.com/mssun/passforios/sshage
+go get github.com/mssun/passforios/sshage/sshage
+
+# Ensure all dependencies are downloaded (go.sum gets updated by tidy/get)
+go mod download all
 mkdir -p dist
 go get golang.org/x/mobile/cmd/gomobile@latest
 go get golang.org/x/mobile/cmd/gobind@latest
@@ -30,7 +57,8 @@ go mod download github.com/ProtonMail/go-crypto
   github.com/ProtonMail/gopenpgp/v2/constants \
   github.com/ProtonMail/gopenpgp/v2/models \
   github.com/ProtonMail/gopenpgp/v2/subtle \
-  github.com/ProtonMail/gopenpgp/v2/helper
+  github.com/ProtonMail/gopenpgp/v2/helper \
+  github.com/mssun/passforios/sshage/sshage/mobile
 popd
 
 cp -r "$GOPENPGP_PATH/dist/Gopenpgp.xcframework" "$OUTPUT_PATH"
